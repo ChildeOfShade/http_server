b92522212024d3a79a60024941af0bb7fe5746ac
TheLaneagen <devinjyork@gmail.com>
2025-11-25 17:57:47 -0600

    We have a working file server

--- main.go
+++ main.go
@@ -5,9 +5,18 @@ import (
 )
 
 func main() {
-    // Serve current directory on /
-    http.Handle("/", http.FileServer(http.Dir(".")))
+    mux := http.NewServeMux()
 
-    // Start server
-    http.ListenAndServe(":8080", nil)
+    // Step 1: readiness endpoint
+    mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+        w.Header().Set("Content-Type", "text/plain; charset=utf-8")
+        w.WriteHeader(http.StatusOK)
+        w.Write([]byte("OK"))
+    })
+
+    // Step 2: fileserver at /app/
+    fileServer := http.FileServer(http.Dir("."))
+    mux.Handle("/app/", http.StripPrefix("/app/", fileServer))
+
+    http.ListenAndServe(":8080", mux)
 }
